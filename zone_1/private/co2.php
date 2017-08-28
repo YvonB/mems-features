b579bc4d68cf3b2a6bba735f0e16c6a1e98b0923
Kevin <yvonbenahita@gmail.com>
2017-08-28 10:55:18 +0300

    correction des urls de rédirection

--- zone_1/private/co2.php
+++ zone_1/private/co2.php
@@ -8,8 +8,206 @@
     if(!$user)
     {   
 
-        header("Location: /"); // on rédirige vers l'accueil et 
-        exit; // On arrête tout.
+        // header("Location: /zone_1"); // on rédirige vers l'accueil et 
+        // exit; // On arrête tout.
+            ?>  
+                    <!DOCTYPE html>
+                    <html>
+                    <head>
+                        <meta charset="utf-8">
+                        <title>Login-SDP</title>
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+                    <style>
+                    /*body*/
+                    body
+                    {
+                       /*body de google*/
+                        background-color: #fafafa !important;
+                        color: rgba(0,0,0,.987) !important;
+                        font-family: 'Roboto',sans-serif !important;
+                        font-size: 12px !important;
+                        font-weight: 400 !important;
+                        letter-spacing: .01em !important;
+                        line-height: 16px !important;
+                        margin: 0 !important;
+                        padding: 0 !important;
+                        overflow: auto !important;
+
+                    }
+
+                    /* The Modal (background) */
+                    .modal {
+                        display: none; /* Hidden by default */
+                        position: fixed; /* Stay in place */
+                        z-index: 1; /* Sit on top */
+                        padding-top: 100px; /* Location of the box */
+                        left: 0;
+                        top: 0;
+                        width: 100%; /* Full width */
+                        height: 100%; /* Full height */
+                        overflow: auto; /* Enable scroll if needed */
+                        background-color: rgb(0,0,0); /* Fallback color */
+                        background-color: rgba(0,0,0,0.7); /* Black w/ opacity */
+                    }
+
+                    /* Modal Content */
+                    .modal-content {
+                        position: relative;
+                        background-color: #fefefe;
+                        margin: auto;
+                        padding: 0;
+                       /* border: 1px solid #888;*/
+                        width: 80%;
+                        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+                        -webkit-animation-name: animatetop;
+                        -webkit-animation-duration: 0.4s;
+                        animation-name: animatetop;
+                        animation-duration: 0.4s
+                    }
+
+                    /* Add Animation */
+                    @-webkit-keyframes animatetop {
+                        from {top:-300px; opacity:0} 
+                        to {top:0; opacity:1}
+                    }
+
+                    @keyframes animatetop {
+                        from {top:-300px; opacity:0}
+                        to {top:0; opacity:1}
+                    }
+
+                    /* The Close Button */
+                    .close {
+                        color: white;
+                        float: right;
+                        font-size: 28px;
+                        font-weight: bold;
+                        margin-top: 14px !important;
+                    }
+
+                    .close:hover,
+                    .close:focus {
+                        color: #000;
+                        text-decoration: none;
+                        cursor: pointer;
+                    }
+
+                    .modal-header {
+                        padding: 2px 16px;
+                        background-color: #37424b;
+                        color: #c8c8c8;
+
+                    }
+
+                    .modal-body 
+                    {
+                        padding: 40px 16px !important;
+                        background-color: #fafafa;
+                        font: 400 16px/24px Roboto, sans-serif;
+                        color: #212121;
+                    }
+                    .okbtn
+                    {
+                        float: right;
+                        margin-right: 15px;
+                    }
+
+                    .mark_ok 
+                    {
+                        background-color: yellow;
+                    }
+
+                    .okbtn:hover
+                    {
+                        background-color: yellow;
+                    }
+
+                    .cancelbtn
+                    {
+                        float: right;
+                    }
+
+                    .mark_cancel 
+                    {
+                        background-color: #e74c3c;
+                    }
+
+                    .cancelbtn:hover
+                    {
+                        background-color: #e74c3c;
+                    }
+
+                    .modal-footer {
+                        padding: 2px 16px;
+                        background-color: #37424b;
+                        color: #c8c8c8;
+                    }
+                    </style>
+                    </head>
+                    <body>
+
+                    <!-- The Modal -->
+                    <div id="myModal" class="modal">
+
+                      <!-- Modal content -->
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <span class="close">&times;</span>
+                            <h3 align="center">SDP - IoT</h3>
+                        </div>
+                        <div class="modal-body">
+                          <p align="center">Une connexion à votre compte est requise pour voir le contenu de la page que vous avez demandée !</p>
+                          <p align="center">Cliuquer sur <mark class="mark_ok">"Ok"</mark> pour <mark class="mark_ok">accepter et continuer</mark> , sinon <mark class="mark_cancel">fermez</mark> cette fenêtre ou cliquez le bouton <mark class="mark_cancel">"Cancel" pour annuler</mark>. Merci !</p>
+                         
+                          <button type="button" onclick="document.location.href='javascript:history.back()'" class="btn btn-default cancelbtn">Cancel</button>
+                           <button type="button" id="ok_btn" class="btn btn-default okbtn">Ok</button>
+
+                        </div>
+                        <div class="modal-footer">
+                          <h4 align="center">© 2017, YvonB All rights reserved</h4>
+                        </div>
+                      </div>
+
+                    </div>
+                    <a href="<?php echo UserService::createLoginURL($_SERVER['REQUEST_URI']) ?>" id="lien"></a>
+                    <script>
+                    // Get the modal
+                    var modal = document.getElementById('myModal');
+
+                    // Get the button that opens the modal
+                    var ok = document.getElementById("ok_btn");
+
+                    // Get the <span> element that closes the modal
+                    var span = document.getElementsByClassName("close")[0];
+
+                    // When the user clicks the button, open the modal 
+                    window.onload = function() {
+                        modal.style.display = "block";
+                    }
+
+                    // When the user clicks on <span> (x), close the modal
+                    span.onclick = function() {
+                        modal.style.display = "none";
+                        document.location.href='javascript:history.back()'; //page précédente
+                    }
+
+                    // When the user clicks anywhere outside of the modal, close it
+                    ok.onclick = function() 
+                    {
+                            modal.style.display = "none";
+                            document.getElementById('lien').click();
+                    }
+
+                    </script>
+
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+                    </body>
+                    </html>
+
+                    <?php   
     }
 
     // Pour notre lib
@@ -62,13 +260,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand colortextnav" href="/"><b>SDP - IoT</b></a>
+              <a class="navbar-brand colortextnav" href="/zone_1"><b>SDP - IoT</b></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
-                <li class="active colortextnav"><a href="#" style="color:#fafafa !important;"><b>CO2</b><span class="sr-only">(current)</span></a></li>
+                <li class="active colortextnav"><a href="" style="color:#fafafa !important;"><b>CO2</b><span class="sr-only">(current)</span></a></li>
               </ul>
               <form class="navbar-form navbar-left" style="margin-left: 150px;">
                 <div class="form-group">
@@ -77,13 +275,13 @@
                 <button type="submit" class="btn btn-default" style="display: none;"><b>Chercher</b></button>
               </form>
               <ul class="nav navbar-nav navbar-right colortextnav">
-                <li><a href="/home"><b><i class="fa fa-home" style="margin-right: 4px;"></i>Back Home</b></a></li>
-                <li><a href="/home/co">Monoxyde de Carbone</a></li>
-                <li><a href="/home/nh3">Amoniaque</a></li>
+                <li><a href="/zone_1/home"><b><i class="fa fa-home" style="margin-right: 4px;"></i>Back Home</b></a></li>
+                <li><a href="/zone_1/home/co">Monoxyde de Carbone</a></li>
+                <li><a href="/zone_1/home/nh3">Amoniaque</a></li>
                 <li class="dropdown colortextnav">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true" style="margin-right: 0px;"></i><b><?php echo htmlspecialchars($user->getNickname());?></b><span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="/login"><button type="submit" class="btn btn-primary" align="center">Se Deconnecter</button></a></li>
+                    <li><a href="/zone_1/login"><button type="submit" class="btn btn-primary" align="center">Se Deconnecter</button></a></li>
                   </ul>
                 </li>
               </ul>
