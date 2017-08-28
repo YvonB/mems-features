109a75968ba8950a4088c51af89e9574a4d1ecda
Kevin <yvonbenahita@gmail.com>
2017-08-28 17:03:00 +0300

    index, reste à faire le Map cliquable

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>SDP - IoT Choix de zones</title>
+	<link rel="stylesheet" href="/css/demo.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 </head>
 <body>
-	<a href="/zone_1">Zone 1</a><a href="/zone_2">Zone 2</a>
+<div class="container"> <!-- contenu de la page -->
+		<div style="margin:auto;width:50%">
+	        <h2><a href="/zone_1">Zonne 1</a></h2> 
+	    </div>
+
+	    <div style="margin:auto;width:50%">
+	        <h2>Zonne 2</h2>
+	            <div class="">
+	                <p>Aie aie aie ! Site encours de préparation... Merci de votre patience !</p>
+	                 <div class="loader_compteurs"></div>
+	            </div>
+	    </div>
+</div> <!-- end contenu -->
 </body>
 </html>
