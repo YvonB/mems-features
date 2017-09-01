0f9d7d9ac75b544a19eadf1579e57cd6cea6366e
Kevin <yvonbenahita@gmail.com>
2017-09-01 08:08:07 +0300

    fix hierco2

--- zone_1/private/hierco2.php
+++ zone_1/private/hierco2.php
@@ -1,17 +1,3 @@
-<?php
-// Inclusion pour notre lib
-require_once('../vendor/autoload.php');
-
-// On crée un objet de type Repository.
-$obj_repo = new \GDS\Demo\Repository();
-// Chercher les 10 dernières valeurs insérées
-$arr_posts = $obj_repo->getAllRecentPost();
-
-if(empty($arr_posts))
-{
-	// loaders
-	?>
-	<!-- html -->
 	<!DOCTYPE html>
 				<html lang="en">
 				<head>
@@ -29,20 +15,6 @@ if(empty($arr_posts))
 				    	body
 						{
 							padding-top: 100px;
-						}
-
-						.loader 
-						{
-						  border: 16px solid #f3f3f3;
-						  border-radius: 50%;
-						  border-top: 16px solid blue;
-						  border-right: 16px solid green;
-						  border-bottom: 16px solid red;
-						  border-left: 16px solid pink;
-						  width: 120px;
-						  height: 120px;
-						  -webkit-animation: spin 2s linear infinite;
-						  animation: spin 2s linear infinite;
 						}
 
 						#results
@@ -81,12 +53,31 @@ if(empty($arr_posts))
 				                    <h2>Quantité de CO2 émis hier</h2>
 				                    <div class="panel panel-default" style="background-color: #cdf;">
 				                        <div class="panel-body">
-											<div class="loader" style="margin:auto;"></div> 
+
+
+
+
+<?php
+// Inclusion pour notre lib
+require_once('../vendor/autoload.php');
+
+// On crée un objet de type Repository.
+$obj_repo = new \GDS\Demo\Repository();
+// Chercher les 10 dernières valeurs insérées
+$arr_posts = $obj_repo->getAllRecentPost();
+
+if(empty($arr_posts))
+{
+	// loaders
+	?>
+	<!-- html -->
+
+											<p align="center"><i class="fa fa-database fa-5" aria-hidden="true"></i></p>
 											<br>
 											<p align="center">Pour l'instant, <strong>aucune donnée </strong> n'est disponnible dans la Base De Données <em>Datastore </em>! </p>
 										</div> <!-- end panel body -->
 									</div> <!-- end panel default -->
-									<a href="/zone_1/home/co2"><button class="btn btn-default retourBtn"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-right: 4px"></i>
+									<a href="/zone_1/home/co2"><button class="btn btn-primary retourBtn"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-right: 4px"></i>
 									Retour</button></a>	
 								</div> <!-- end col md 8  -->
 							</div> <!-- end row -->
@@ -114,61 +105,6 @@ else
 					{
 						//on affiche tous les valeurs de co2
 						?>
-							<!DOCTYPE html>
-							<html lang="en">
-							<head>
-								<meta charset="UTF-8">
-								<title>Document</title>
-								
-								<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-							    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-								<!-- font awesome -->
-							    <link rel="stylesheet" href="css/font-awesome/font-awesome.css">
-
-							     <link rel="stylesheet" href="/css/demo.css">
-
-							    <style>
-							    	body
-									{
-										padding-top: 100px;
-									}
-
-									#results
-									{
-										margin:auto !important;
-										width: 70% !important;
-										float: none !important;
-
-										box-shadow: 0 0 10px;
-										height: 310px;
-									}
-
-									.retourBtn
-									{
-										float: right;
-										margin-right: 15px;
-									}
-
-									@-webkit-keyframes spin {
-									  0% { -webkit-transform: rotate(0deg); }
-									  100% { -webkit-transform: rotate(360deg); }
-									}
-
-									@keyframes spin {
-									  0% { transform: rotate(0deg); }
-									  100% { transform: rotate(360deg); }
-									}
-								</style>
-							</head>
-							<body>
-
-							<div class="container"> <!-- contenu de la page -->
-
-							            <div class="row">
-							                <div class="col-md-8" id="results">
-							                    <h2>Quantité de CO2 émis hier</h2>
-							                    <div class="panel panel-default" style="background-color: #cdf;">
-							                        <div class="panel-body">
 										<?php
 							
 										                // On affiche les résultats pour le CO2
@@ -182,7 +118,7 @@ else
 										?>
 													</div> <!-- end panel body -->
 												</div> <!-- end panel default -->
-									<a href="/zone_1/home/co2"><button class="btn btn-default retourBtn"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-right: 4px"></i>
+									<a href="/zone_1/home/co2"><button class="btn btn-primary retourBtn"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-right: 4px"></i>
 									Retour</button></a>	
 											</div> <!-- col md 8 et results -->
 										</div> <!-- end row -->
@@ -201,62 +137,7 @@ else
 				    {	
 				    	?>
 				    <!-- html -->
-				    		<!DOCTYPE html>
-							<html lang="en">
-							<head>
-								<meta charset="UTF-8">
-								<title>Document</title>
-								
-								<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-							    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-								<!-- font awesome -->
-							    <link rel="stylesheet" href="css/font-awesome/font-awesome.css">
-
-							     <link rel="stylesheet" href="/css/demo.css">
-
-							    <style>
-							    	body
-									{
-										padding-top: 100px;
-									}
-
-									#results
-									{
-										margin:auto !important;
-										width: 70% !important;
-										float: none !important;
-
-										box-shadow: 0 0 10px;
-										height: 265px;
-
-									}
-
-									.retourBtn
-									{
-										float: right;
-										margin-right: 15px;
-									}
-
-									@-webkit-keyframes spin {
-									  0% { -webkit-transform: rotate(0deg); }
-									  100% { -webkit-transform: rotate(360deg); }
-									}
-
-									@keyframes spin {
-									  0% { transform: rotate(0deg); }
-									  100% { transform: rotate(360deg); }
-									}
-								</style>
-							</head>
-							<body>
-
-							<div class="container"> <!-- contenu de la page -->
-
-							            <div class="row">
-							                <div class="col-md-8" id="results">
-							                    <h2>Quantité de CO2 émis hier</h2>
-							                    <div class="panel panel-default" style="background-color: #cdf;">
-							                        <div class="panel-body">
+				    		
 				    <?php
 				    	// On n'a pas trouvé aucune correspondance
 						echo '<div class="post">';
@@ -270,7 +151,7 @@ else
 						<!-- html -->
 						</div> <!-- end panel body -->
 												</div> <!-- end panel default -->
-									<a href="/zone_1/home/co2"><button class="btn btn-default retourBtn"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-right: 4px"></i>
+									<a href="/zone_1/home/co2"><button class="btn btn-primary retourBtn"><i class="fa fa-arrow-left" aria-hidden="true" style="margin-right: 4px"></i>
 									Retour</button></a>	
 											</div> <!-- col md 8 et results -->
 										</div> <!-- end row -->
