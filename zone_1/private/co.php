8c2164c0f057a4ce699d672cd484432e26c9c5f1
Kevin <yvonbenahita@gmail.com>
2017-09-04 09:05:49 +0300

    Formating: identation co

--- zone_1/private/co.php
+++ zone_1/private/co.php
@@ -7,207 +7,195 @@
     // on a pas le droit de voir index si on était pas connecter au préalable.
     if(!$user)
     {   
+        ?>  
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="utf-8">
+                <title>Login-SDP</title>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+                <!-- CSS global -->
+                <link rel="stylesheet" href="/css/demo.css">
+            
+            <style>
 
-        // header("Location: /zone_1"); // on rédirige vers l'accueil et 
-        // exit; // On arrête tout.
-            ?>  
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Login-SDP</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-     <!-- CSS global -->
-    <link rel="stylesheet" href="/css/demo.css">
-<style>
-/*body*/
-body
-{
-   /*body de google*/
-    background-color: #fafafa !important;
-    color: rgba(0,0,0,.987) !important;
-    font-family: 'Roboto',sans-serif !important;
-    font-size: 12px !important;
-    font-weight: 400 !important;
-    letter-spacing: .01em !important;
-    line-height: 16px !important;
-    margin: 0 !important;
-    padding: 0 !important;
-    overflow: auto !important;
+                /*body*/
+                body
+                {
+                   /*body de google*/
+                    background-color: #fafafa !important;
+                    color: rgba(0,0,0,.987) !important;
+                    font-family: 'Roboto',sans-serif !important;
+                    font-size: 12px !important;
+                    font-weight: 400 !important;
+                    letter-spacing: .01em !important;
+                    line-height: 16px !important;
+                    margin: 0 !important;
+                    padding: 0 !important;
+                    overflow: auto !important;
+                }
 
-}
+                /* The Modal (background) */
+                .modal 
+                {
+                    display: none; /* Hidden by default */
+                    position: fixed; /* Stay in place */
+                    z-index: 1; /* Sit on top */
+                    padding-top: 100px; /* Location of the box */
+                    left: 0;
+                    top: 0;
+                    width: 100%; /* Full width */
+                    height: 100%; /* Full height */
+                    overflow: auto; /* Enable scroll if needed */
+                    background-color: rgb(0,0,0); /* Fallback color */
+                    background-color: rgba(0,0,0,0.7); /* Black w/ opacity */
+                }
 
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.7); /* Black w/ opacity */
-}
+                /* Modal Content */
+                .modal-content 
+                {
+                    position: relative;
+                    background-color: #fefefe;
+                    margin: auto;
+                    padding: 0;
+                   /* border: 1px solid #888;*/
+                    width: 80%;
+                    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+                    -webkit-animation-name: animatetop;
+                    -webkit-animation-duration: 0.4s;
+                    animation-name: animatetop;
+                    animation-duration: 0.4s
+                }
 
-/* Modal Content */
-.modal-content {
-    position: relative;
-    background-color: #fefefe;
-    margin: auto;
-    padding: 0;
-   /* border: 1px solid #888;*/
-    width: 80%;
-    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
-    -webkit-animation-name: animatetop;
-    -webkit-animation-duration: 0.4s;
-    animation-name: animatetop;
-    animation-duration: 0.4s
-}
+                /* Add Animation */
+                @-webkit-keyframes animatetop {
+                    from {top:-300px; opacity:0} 
+                    to {top:0; opacity:1}
+                }
 
-/* Add Animation */
-@-webkit-keyframes animatetop {
-    from {top:-300px; opacity:0} 
-    to {top:0; opacity:1}
-}
+                @keyframes animatetop {
+                    from {top:-300px; opacity:0}
+                    to {top:0; opacity:1}
+                }
 
-@keyframes animatetop {
-    from {top:-300px; opacity:0}
-    to {top:0; opacity:1}
-}
+                /* The Close Button */
+                .close 
+                {
+                    color: white;
+                    float: right;
+                    font-size: 28px;
+                    font-weight: bold;
+                    margin-top: 14px !important;
+                }
 
-/* The Close Button */
-.close {
-    color: white;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-    margin-top: 14px !important;
-}
+                .close:hover,
+                .close:focus
+                {
+                    color: #000;
+                    text-decoration: none;
+                    cursor: pointer;
+                }
 
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
+                .modal-header 
+                {
+                    padding: 2px 16px;
+                    background-color: rgb(59, 120, 231);
+                    color: #c8c8c8;
 
-.modal-header {
-    padding: 2px 16px;
-    background-color: rgb(59, 120, 231);
-    color: #c8c8c8;
+                }
 
-}
+                .modal-body 
+                {
+                    padding: 40px 16px !important;
+                    background-color: #fafafa;
+                    font: 400 16px/24px Roboto, sans-serif;
+                    color: #212121;
+                }
 
-.modal-body 
-{
-    padding: 40px 16px !important;
-    background-color: #fafafa;
-    font: 400 16px/24px Roboto, sans-serif;
-    color: #212121;
-}
-.okbtn
-{
-    float: left;
-    margin-right: 15px;
-}
+                .okbtn
+                {
+                    float: left;
+                    margin-right: 15px;
+                }
 
-/*.mark_ok 
-{
-    background-color: yellow;
-}*/
+                .cancelbtn
+                {
+                    float: right;
+                }
 
-/*.okbtn:hover
-{
-    background-color: yellow;
-}*/
+                .modal-footer 
+                {
+                    padding: 2px 16px;
+                    background-color: rgb(59, 120, 231);
+                    color: #c8c8c8;
+                }
+            </style>
 
-.cancelbtn
-{
-    float: right;
-}
+            </head>
+            
+            <body>
 
-/*.mark_cancel 
-{
-    background-color: #e74c3c;
-}*/
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
 
-/*.cancelbtn:hover
-{
-    background-color: #e74c3c;
-}
-*/
-.modal-footer {
-    padding: 2px 16px;
-    background-color: rgb(59, 120, 231);
-    color: #c8c8c8;
-}
-</style>
-</head>
-<body>
+              <!-- Modal content -->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <span class="close">&times;</span>
+                    <h3 align="center" style="color:#fafafa;">SDP - IoT</h3>
+                </div>
+                <div class="modal-body">
+                  <p align="center">Une connexion à votre compte est requise pour voir le contenu de la page que vous avez demandée !</p>
+                  <p align="center">Cliuquer sur <mark class="mark_ok">"Accept"</mark> pour <mark class="mark_ok">accepter et continuer</mark> , sinon <mark class="mark_cancel">fermez</mark> cette fenêtre ou cliquez le bouton <mark class="mark_cancel">"Cancel" pour annuler</mark>. Merci !</p>
+                 
+                  <button type="button" onclick="document.location.href='javascript:history.back()'" class="btn btn-primary cancelbtn">Cancel</button>
+                   <button type="button" id="ok_btn" class="btn btn-primary okbtn">Accept</button>
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
+                </div>
+                <div class="modal-footer">
+                  <h4 align="center" style="color:#fafafa;">© 2017, YvonB All rights reserved</h4>
+                </div>
+              </div>
 
-  <!-- Modal content -->
-  <div class="modal-content">
-    <div class="modal-header">
-      <span class="close">&times;</span>
-        <h3 align="center" style="color:#fafafa;">SDP - IoT</h3>
-    </div>
-    <div class="modal-body">
-      <p align="center">Une connexion à votre compte est requise pour voir le contenu de la page que vous avez demandée !</p>
-      <p align="center">Cliuquer sur <mark class="mark_ok">"Accept"</mark> pour <mark class="mark_ok">accepter et continuer</mark> , sinon <mark class="mark_cancel">fermez</mark> cette fenêtre ou cliquez le bouton <mark class="mark_cancel">"Cancel" pour annuler</mark>. Merci !</p>
-     
-      <button type="button" onclick="document.location.href='javascript:history.back()'" class="btn btn-primary cancelbtn">Cancel</button>
-       <button type="button" id="ok_btn" class="btn btn-primary okbtn">Accept</button>
+            </div>
 
-    </div>
-    <div class="modal-footer">
-      <h4 align="center" style="color:#fafafa;">© 2017, YvonB All rights reserved</h4>
-    </div>
-  </div>
+            <a href="<?php echo UserService::createLoginURL($_SERVER['REQUEST_URI']) ?>" id="lien"></a>
 
-</div>
-<a href="<?php echo UserService::createLoginURL($_SERVER['REQUEST_URI']) ?>" id="lien"></a>
-<script>
-// Get the modal
-var modal = document.getElementById('myModal');
+            <script>
+            // Get the modal
+            var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var ok = document.getElementById("ok_btn");
+            // Get the button that opens the modal
+            var ok = document.getElementById("ok_btn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-window.onload = function() {
-    modal.style.display = "block";
-}
+            // When the user clicks the button, open the modal 
+            window.onload = function() {
+                modal.style.display = "block";
+            }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-    document.location.href='javascript:history.back()'; //page précédente
-}
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+                document.location.href='javascript:history.back()'; //page précédente
+            }
 
-// When the user clicks anywhere outside of the modal, close it
-ok.onclick = function() 
-{
-        modal.style.display = "none";
-        document.getElementById('lien').click();
-}
+            // When the user clicks anywhere outside of the modal, close it
+            ok.onclick = function() 
+            {
+                    modal.style.display = "none";
+                    document.getElementById('lien').click();
+            }
+            </script>
 
-</script>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-</body>
-</html>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+            </body>
+            </html>
 
 <?php   
     }
@@ -215,9 +203,10 @@ ok.onclick = function()
     // Pour notre lib
     require_once('../vendor/autoload.php');
 
-    // Chercher les dernières valeurs insérées
+    // On crée un objet de type Repository.
     $obj_repo = new \GDS\Demo\Repository();
-    $arr_posts = $obj_repo->getRecentPosts();
+    // Chercher juste la dernières valeurs insérées récemment.
+    $arr_posts = $obj_repo->getLatestRecentPost();
 
 ?>
 
@@ -230,10 +219,8 @@ ok.onclick = function()
     <meta name="viewport" content="width=device-width, initial-scale=1">    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="/css/demo.css">
     <link rel="stylesheet" href="/css/slide.css">  <!-- pour l'utilisation de #slider ect -->
-
     <meta name="author" content="Yvon Benahita">
     <link rel="icon" type="image/png" href="/img/datastore-logo.png" />
 
@@ -331,15 +318,6 @@ ok.onclick = function()
     <?php
         try
             {   
-                // ========Appel de notre modèle
-
-                // On crée un objet de type Repository.
-                $obj_repo = new \GDS\Demo\Repository();
-                // Chercher juste la dernières valeurs insérées récemment.
-                $arr_posts = $obj_repo->getLatestRecentPost();
-
-                // =========fin appel de notre modèle
-
                // val ppm
                 if(isset($arr_posts)){$ppm_co = $arr_posts->co;} 
             }
