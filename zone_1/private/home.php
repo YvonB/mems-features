a4c7bb5d6c9c71361d5e843eedf3c8a93700990a
Kevin <yvonbenahita@gmail.com>
2017-09-04 09:18:25 +0300

    Formating: identation home

--- zone_1/private/home.php
+++ zone_1/private/home.php
@@ -28,21 +28,18 @@
 <!-- head -->
 <head>
     <meta charset="utf-8">
-    <title>Détéction de pollution</title>
+    <title>SDPE - IoT</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-
     <!-- CSS global -->
     <link rel="stylesheet" href="/css/demo.css">
     <!-- css du slide -->
     <link rel="stylesheet" href="/css/slide.css">
     <!-- fin css slide -->
-
     <meta name="author" content="Yvon Benahita">
     <link rel="icon" type="image/png" href="/img/datastore-logo.png" />
-
     <!-- font -->
     <link rel="stylesheet" href="css/font-awesome/font-awesome.css">
 
