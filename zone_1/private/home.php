27aba86be90d2816be220d7f91f8ac91e9dc93fd
Kevin <yvonbenahita@gmail.com>
2017-09-06 17:28:50 +0300

    envoi mail réussi, reste à faire le chiffre qui augmente auto sur le logo de gmail

--- zone_1/private/home.php
+++ zone_1/private/home.php
@@ -16,9 +16,96 @@
 
     // pour cacher les notices au niveau des compteurs lorsque les valeures ne sont pas encore dispo
     ini_set("display_errors",0);error_reporting(0);
-    // Chercher les dernières valeurs insérées
+    
     $obj_repo = new \GDS\Demo\Repository();
-    $arr_posts = $obj_repo->getRecentPosts();
+    // $arr_posts = $obj_repo->getRecentPosts();
+
+    // Chercher TOUS 'All' les gazs insérées récemment.
+    $arr_posts = $obj_repo->getAllRecentPost();
+
+    //global
+    $count_mail = 0;
+    $notif_mail = 0;
+
+    // au début 
+    $nbr_co2_na = 0;
+    $n_co2 = 0;
+    $nbr_co_na = 0;
+    $n_co = 0;
+    $nbr_nh3_na = 0;
+    $n_nh3 = 0; 
+
+    // Compte tous les posts.
+    $nbr = count($arr_posts); // C'est le N dans le livre
+                        
+    foreach ($arr_posts as $obj_post) 
+    {
+        if($obj_post->co2 >= 396)// tous les co2 qui dépasse ou égale à 396ppm
+            {   
+                $nbr_co2_na += 1; // si on est ici c'est qu'il y a des co2 non acceptables, on icremente le nombre $nbr_co2_na alors !
+                $n_co2 = $nbr_co2_na;
+                // $co2_na = $obj_post->co2;
+            }
+        if($obj_post->co >= 3) // tous les co qui dépasse ou égale à 3ppm
+            {
+                // si on est ici c'est qu'il y a des co non acceptables, on icremente le nombre $nbr_co_na alors !
+                $nbr_co_na += 1;
+                $n_co = $nbr_co_na;
+                // $co_na = $obj_post->co;
+            }
+        if($obj_post->nh3 >= 5) // tous les nh3 qui dépasse ou égale à 5ppm
+                {   
+                    // si on est ici c'est qu'il y a des nh3 non acceptables, on icremente le nombre $nbr_nh3_na alors !
+                    $nbr_nh3_na += 1;
+                    $n_nh3 = $nbr_nh3_na;
+                }  
+      }
+
+      //calculs les %
+      if($nbr != 0) // on évite la division par zéro
+          {   
+              $pource_co2 = ($n_co2*100)/$nbr;
+              $pource_co = ($n_co*100)/$nbr;
+              $pource_nh3 = ($n_nh3*100)/$nbr;
+          }
+
+      $curentUserMail = htmlspecialchars($user->getEmail()); 
+
+                           
+      // envoyer un mail à l'utilisatuer courante                    
+      if($pource_co2 > 50)
+        {
+          $to = $curentUserMail;
+          $subject = "Alert au Gaz carbonique";
+          $txt = "Le taux de Gaz carbonique non acceptable est de: ".$pource_co2."%.";
+          $headers = "From: sdpeiot@mems-6-3.appspotmail.com" . "\r\n";
+                              
+          mail($to,$subject,$txt,$headers);
+        }
+      if ($pource_co > 50) 
+        {
+          $to = $curentUserMail;
+          $subject = "Alert au Monoxyde de Carbone";
+          $txt = "Le taux de Monoxyde de Carbone non acceptable est de: ".$pource_co."%.";
+          $headers = "From: sdpeiot@mems-6-3.appspotmail.com" . "\r\n";
+                              
+          mail($to,$subject,$txt,$headers);
+        }
+      if ($pource_nh3 > 50) 
+          {
+            $to = $curentUserMail;
+            $subject = "Alert à l' Ammoniaque";
+            $txt = "Le taux d'Amoniaque non acceptable est de: ".$pource_nh3."%.";
+            $headers = "From: sdpeiot@mems-6-3.appspotmail.com" . "\r\n";
+                              
+            mail($to,$subject,$txt,$headers);
+          } 
+            // fin envoye mail 
+
+            function NbrMail()
+              {
+                return $notif_mail;
+              }
 
 ?>
 
@@ -67,7 +154,7 @@
             <h3><?php echo  date('l jS \of F Y'); ?></h3>
           </div> <!-- end date -->
 
-          <br><img src="/img/gmail.png" id="gmail-logo" /><b><?php echo "2"; ?></b>
+          <br><img src="/img/gmail.png" id="gmail-logo" /><b><?php echo $notif_mail; ?></b>
 
            <!-- affiche heure -->
           <script type="text/javascript">
@@ -143,105 +230,6 @@
                     <h2 align="center"><i class="fa fa-pie-chart" style="margin-right: 8px" aria-hidden="true"></i>Gas not accepted</h2>
                       <div id="container" style="width:100%;height: 400px"> <!-- div pour contenir le Pie -->
 
-                      <?php
-
-                        try
-                        {
-                        // On crée un objet de type Repository.
-                        $obj_repo = new \GDS\Demo\Repository();
-                        // Chercher TOUS 'All' les gazs insérées récemment.
-                        $arr_posts = $obj_repo->getAllRecentPost();
-
-                        // au début 
-                        $nbr_co2_na = 0;
-                        $n_co2 = 0;
-                        $nbr_co_na = 0;
-                        $n_co = 0;
-                        $nbr_nh3_na = 0;
-                        $n_nh3 = 0;   
-
-                        // Compte tous les posts.
-                        $nbr = count($arr_posts); // C'est le N dans le livre
-                        
-                        foreach ($arr_posts as $obj_post) 
-                        {
-                            if($obj_post->co2 >= 396)// tous les co2 qui dépasse ou égale à 396ppm
-                                {   
-                                    $nbr_co2_na += 1; // si on est ici c'est qu'il y a des co2 non acceptables, on icremente le nombre $nbr_co2_na alors !
-                                    $n_co2 = $nbr_co2_na;
-                                    // $co2_na = $obj_post->co2;
-                                }
-                            if($obj_post->co >= 3) // tous les co qui dépasse ou égale à 3ppm
-                                {
-                                    // si on est ici c'est qu'il y a des co non acceptables, on icremente le nombre $nbr_co_na alors !
-                                    $nbr_co_na += 1;
-                                    $n_co = $nbr_co_na;
-                                    // $co_na = $obj_post->co;
-                                }
-                            if($obj_post->nh3 >= 5) // tous les nh3 qui dépasse ou égale à 5ppm
-                                    {   
-                                        // si on est ici c'est qu'il y a des nh3 non acceptables, on icremente le nombre $nbr_nh3_na alors !
-                                        $nbr_nh3_na += 1;
-                                        $n_nh3 = $nbr_nh3_na;
-                                    }  
-                            ?>
-                        
-                        <?php 
-
-                        }
-
-
-
-                        //calculs les %
-                        if($nbr != 0) // on évite la division par zéro
-                            {   
-                                $pource_co2 = ($n_co2*100)/$nbr;
-                                $pource_co = ($n_co*100)/$nbr;
-                                $pource_nh3 = ($n_nh3*100)/$nbr;
-                            }
-                        ?>
-                        
-                        <?php
-
-                           $curentUserMail = htmlspecialchars($user->getEmail()); 
-
-                           
-                        // envoyer un mail à l'utilisatuer courante
-                           
-                                if($pource_co2 > 50)
-                                  {
-                                    $to = $curentUserMail;
-                                    $subject = "Alert au Gaz carbonique";
-                                    $txt = "Le taux de Gaz carbonique non acceptable est de: ".$pource_co2."%.";
-                                    $headers = "From: sdpeiot@mems-6-2.appspotmail.com" . "\r\n";
-                              
-                                    mail($to,$subject,$txt,$headers);
-                                  }
-                                  elseif ($pource_co > 50) 
-                                    {
-                                      $to = $curentUserMail;
-                                      $subject = "Alert au Monoxyde de Carbone";
-                                      $txt = "Le taux de Monoxyde de Carbone non acceptable est de: ".$pource_co."%.";
-                                      $headers = "From: sdpeiot@mems-6-2.appspotmail.com" . "\r\n";
-                              
-                                      mail($to,$subject,$txt,$headers);
-                                    }
-                                    elseif ($pource_co > 50) 
-                                      {
-                                        $to = $curentUserMail;
-                                        $subject = "Alert à l' Ammoniaque";
-                                        $txt = "Le taux d'Amoniaque non acceptable est de: ".$pource_nh3."%.";
-                                        $headers = "From: sdpeiot@mems-6-2.appspotmail.com" . "\r\n";
-                              
-                                        mail($to,$subject,$txt,$headers);
-                                      }
-                                    
-
-                                
-                              
-                                
-                            // fin envoye mail 
-                          ?>    
                         <!-- // Tant que les données ne sont pas prêtes on affiche un loader   -->
                          <?php 
                         
@@ -322,16 +310,6 @@
                                 }
                             </script>
                         <!-- end actu auto -->
-
-                          <?php
-                   }
-                    catch(\Exception $obj_ex)
-                        {
-                            syslog(LOG_ERR, $obj_ex->getMessage());
-                            echo '<em>Whoops, something went wrong!</em>';
-                        }
-
-                            ?>
                    </div> <!-- fin div Pie -->
                 </div> <!-- end coll md 4 -->
             </div> <!-- end row -->
