8e3e189d7e341b9a165d7a34bbb46f86b1c48073
Kevin <yvonbenahita@gmail.com>
2017-09-07 16:54:50 +0300

    Refactoring ok jusqu'ici

--- zone_1/private/home.php
+++ zone_1/private/home.php
@@ -13,43 +13,7 @@
 
     ini_set("display_errors",0);error_reporting(0);
  
-    $curentUserMail = htmlspecialchars($user->getEmail()); 
-
-      // Demander des données au serveur
-      require_once '../private/live-server-main-pourc-not-acceptable.php';
-                           
-      // envoyer un mail à l'utilisatuer courante                    
-      if($res[0] > 50)
-        {
-          $to = $curentUserMail;
-          $subject = "Alert au Gaz carbonique";
-          $txt = "Le taux de Gaz carbonique non acceptable est de: ".$res[0]."%.";
-          $headers = "From: sdpeiot@mems-7-3-179211.appspotmail.com" . "\r\n";
-                              
-          mail($to,$subject,$txt,$headers);
-          $notif_mail++;
-        }
-      if ($res[1] > 50) 
-        {
-          $to = $curentUserMail;
-          $subject = "Alert au Monoxyde de Carbone";
-          $txt = "Le taux de Monoxyde de Carbone non acceptable est de: ".$res[1]."%.";
-          $headers = "From: sdpeiot@mems-7-3-179211.appspotmail.com" . "\r\n";
-                              
-          mail($to,$subject,$txt,$headers);
-          $notif_mail++;
-        }
-      if ($res[2] > 50) 
-          {
-            $to = $curentUserMail;
-            $subject = "Alert à l' Ammoniaque";
-            $txt = "Le taux d'Amoniaque non acceptable est de: ".$res[2]."%.";
-            $headers = "From: sdpeiot@mems-7-3-179211.appspotmail.com" . "\r\n";
-                              
-            mail($to,$subject,$txt,$headers);
-            $notif_mail++;
-          } 
-            // fin envoye mail 
+    require_once 'sendmail.php';
 ?>
 
 <!DOCTYPE html>
@@ -58,7 +22,7 @@
 <!-- head -->
 <head>
     <meta charset="utf-8">
-    <title>SDPE - IoT</title>
+    <title>SDPE - IoT Home</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
