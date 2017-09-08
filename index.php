2af6b45dd47df7797f3bcd1e772f1365ca06e683
Kevin <yvonbenahita@gmail.com>
2017-09-08 09:07:49 +0300

    css de index dans un autre fichier

--- index.php
+++ index.php
@@ -1,57 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
-	<title>SDP - IoT Choix de zones</title>
+	<title>SDPE - IoT Choix de zones</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/css/demo.css">
+	<link rel="stylesheet" href="/css/index.css">
 	<link rel="icon" type="image/png" href="/img/datastore-logo.png" />
-	<style type="text/css">
-		
-		body
-		{
-			padding-top: 100px;
-		}
 
-		.global .zone_1 
-			{
-	    		float:left;
-	    		width:45%;
-
-	    		box-shadow: 0 0 10px;
-			}
-		.global .zone_2 
-			{
-    			float:right; 
-    			width:45%;
-
-    			box-shadow: 0 0 10px;
-			}
-
-		#map 
-			{
-		        width: 100%;
-		        height: 400px;
-		        background-color: grey;
-     	 	}
-
-     	.titre_zones
-     	{
-     		text-align: center;
-     	}
-
-		a
-		{
-			color: #039be5 !important;
-		}
-
-        a:hover
-     	{
-     		color: #337ab7 !important;
-     		text-decoration: none !important;
-     	}
-
-	</style>
 </head>
 <body>
 <div class="container"> <!-- contenu de la page -->
