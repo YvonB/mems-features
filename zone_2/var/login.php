273ba149476343580f4fa541255061cd3e2d6468
Kevin <yvonbenahita@gmail.com>
2017-09-13 08:56:01 +0300

    fix urls var

--- zone_2/var/login.php
+++ zone_2/var/login.php
@@ -9,7 +9,7 @@ $user = UserService::getCurrentUser();
 if ($user) 
 {
     // echo 'Hello, ' . htmlspecialchars($user->getNickname());
-    header('Location: /zone_1/home');
+    header('Location: /zone_2/home');
 }
 // [END ifuser]
 // [START elseuser]
