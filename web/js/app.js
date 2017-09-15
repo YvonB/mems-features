f4618ed300443d3369ca538901c424f711992ea9
Kevin <yvonbenahita@gmail.com>
2017-09-15 15:40:03 +0300

    ajout d'un popUp d'info aux marqueurs

--- web/js/app.js
+++ web/js/app.js
@@ -5,86 +5,7 @@
 	  var map = new google.maps.Map(document.getElementById('map'), {
 	    zoom: 12,
 	    center: {lat: -18.9413966, lng: 47.46252389999995}, // Ambodiafontsy
-	    styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
 	  });
 
 	  // premier marqueur == première zone
@@ -95,7 +16,17 @@
 	 	url: 'https://zone-1-179812.appspot.com'
 	 
 	 });
-	 
+
+	 //info sur lui
+	var content_cur = '<h1>Cur Vontovorona</h1><br><h2>Première zone</h2>';
+	var info_cur = new google.maps.InfoWindow({
+		content:content_cur
+	});
+	// on affiche cette info lorsque la page est chargée
+	function popUpCur(){
+		info_cur.open(map, cur);
+	}
+
 	 // lorsqu'on clique dessus
 	 google.maps.event.addListener(cur, 'click', function() {
 	  window.location.href = cur.url;
@@ -109,9 +40,21 @@
 	  	url: 'https://zone-2-179812.appspot.com'
 	  });
 
+	 //info sur lui
+	var content_tunnel = '<h1>Tunnel Ambanidia</h1><br><h2>Seconde zone</h2>';
+	var info_tunnel = new google.maps.InfoWindow({
+		content:content_tunnel
+	});
+		// on affiche cette info lorsque la page est chargée
+	 function popUpTunnel(){
+		info_tunnel.open(map, tunnel);
+	}
 	 // lorsqu'on clique dessus
 	  google.maps.event.addListener(tunnel, 'click', function() {
 	  window.location.href = tunnel.url;
 	  });
+
+	popUpCur();
+	popUpTunnel();
 	
 })();	
