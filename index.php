1a9701bb89b644ca4327ce26f1586924c9fca7ae
Kevin <yvonbenahita@gmail.com>
2017-09-15 11:14:17 +0300

    map ajouté

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>SDPE - IoT Choix de zones</title>
-	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
-   integrity="sha512-M2wvCLH6DSRazYeZRIm1JnYyh22purTM+FDB5CsyxtQJYeKq83arPe5wgbNmcFXGqiSH2XR8dT/fJISVA1r/zQ=="
-   crossorigin=""/>
+	
 	<link rel="stylesheet" href="/css/index.css">
 	<link rel="icon" type="image/png" href="/img/datastore-logo.png" />
 
@@ -14,11 +12,14 @@
 <body>
         
 	<div id="map"></div>
+ <!-- Replace the value of the key parameter with your own API key. -->
 
-<!-- Make sure you put this AFTER Leaflet's CSS -->
- <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"
-   integrity="sha512-lInM/apFSqyy1o6s89K4iQUKg6ppXEgsVxT35HbzUupEVRh2Eu9Wdl4tHj7dZO0s1uvplcYGmt3498TtHq+log=="
-   crossorigin=""></script>
 <script type="text/javascript" src="/js/app.js"></script>
+<script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+</script>
+
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMtlCONmXj3VunLv9V4UpSlFFLFgCNPJ0&callback=initMap">
+    </script>
 </body>
 </html>
