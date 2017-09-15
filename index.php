2af19307bc15da65e4786600c5f2848b2b0e6916
Kevin <yvonbenahita@gmail.com>
2017-09-15 14:04:17 +0300

    tend vers poinsa

--- index.php
+++ index.php
@@ -12,14 +12,11 @@
 <body>
         
 	<div id="map"></div>
- <!-- Replace the value of the key parameter with your own API key. -->
 
-<script type="text/javascript" src="/js/app.js"></script>
-<script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
-</script>
 
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMtlCONmXj3VunLv9V4UpSlFFLFgCNPJ0&callback=initMap">
+<script 
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMtlCONmXj3VunLv9V4UpSlFFLFgCNPJ0">
     </script>
+    <script type="text/javascript" src="/js/app.js"></script>
 </body>
 </html>
