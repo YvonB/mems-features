f2edb06ad7fedb71a68128727f2307de5938a75f
Kevin <yvonbenahita@gmail.com>
2017-09-19 16:11:23 +0300

    pop up ajouté sur le map de choix de zones

--- index.php
+++ index.php
@@ -13,8 +13,19 @@
         
 	<div id="map"></div>
 
+	<!-- The Modal -->
+	<div id="myModal" class="modal">
 
-<script 
+	  <!-- Modal content -->
+	  <div class="modal-content">
+	    <span class="close">&times;</span>
+	    <p>Veuillez choisir une zone en cliquant sur l'un des marqueurs. Merci</p>
+	  </div>
+
+	</div>
+
+
+	<script 
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMtlCONmXj3VunLv9V4UpSlFFLFgCNPJ0">
     </script>
     <script type="text/javascript" src="/js/app.js"></script>
--- web/js/app.js
+++ web/js/app.js
@@ -13,10 +13,24 @@
 	    position: {lat: -18.9703576 , lng: 47.42391939999993},
 	 
 	 	map: map,
-	 	url: 'https://zone-1-179812.appspot.com'
+	 	url: 'https://vontovorna.appspot.com',
+	 	draggable: true,
+	 	animation: google.maps.Animation.DROP
 	 
 	 });
-
+	  // fonction pour animer le marker
+	  function toggleBounceCur()
+	   {
+  			if (cur.getAnimation() !== null) 
+	  			{
+	    			cur.setAnimation(null);
+	  			} 
+  			else 
+	  			{
+	    			cur.setAnimation(google.maps.Animation.BOUNCE);
+	  			}
+		}
+	cur.addListener('onload', toggleBounceCur);
 	 //info sur lui
 	var content_cur = '<h1 class="titre">Cur Vontovorona</h1><h2 class="sous_titre">Première zone</h2>';
 	var info_cur = new google.maps.InfoWindow({
@@ -36,9 +50,23 @@
 	    position: {lat: -18.914834, lng: 47.531656699999985},
 	 
 	    map: map,
-	  	url: 'https://zone-2-179812.appspot.com'
+	  	url: 'https://zone-two.appspot.com',
+	  	draggable: true,
+	 	animation: google.maps.Animation.DROP
 	  });
-
+	 // fonction pour animer le marker
+	  function toggleBounceTunnel()
+	   {
+  			if (tunnel.getAnimation() !== null) 
+	  			{
+	    			tunnel.setAnimation(null);
+	  			} 
+  			else 
+	  			{
+	    			tunnel.setAnimation(google.maps.Animation.BOUNCE);
+	  			}
+		}
+	tunnel.addListener('onload', toggleBounceTunnel);
 	 //info sur lui
 	var content_tunnel = '<h1 class="titre">Tunnel Ambanidia</h1><h2 class="sous_titre">Seconde zone</h2>';
 	var info_tunnel = new google.maps.InfoWindow({
@@ -55,5 +83,36 @@
 
 	popUpCur();
 	popUpTunnel();
+
 	
+	// ============= modal
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	//  open the modal 
+	function afficheModal() {
+	    modal.style.display = "block";
+	}
+	// close the modal
+	function cacheModal() {
+	    modal.style.display = "none";
+	}
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+	// ============= end modal
+
+	setTimeout(afficheModal, 5000);
+	setTimeout(cacheModal, 10000);
 })();	
