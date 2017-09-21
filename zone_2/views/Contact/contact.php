19594f1fb61e37d5332abaf579e2696677c4feac
Kevin <yvonbenahita@gmail.com>
2017-09-21 16:50:34 +0300

    formulaire de commande bien styliser

--- zone_2/views/Contact/contact.php
+++ zone_2/views/Contact/contact.php
@@ -19,19 +19,19 @@
 	<div class="panel-heading" style="font-size: 20px;">Envoyer nous votre commande</div>
 	<div class="panel-body">
 	<form id="contact" method="post" action="/contact_action">
-	<fieldset class="col-md-6"><legend>Vos coordonnées</legend>
-		<div class="form-group"><label for="nom">Nom :</label><input class="form-control input_cmd" type="text" id="nom" name="nom" tabindex="1" /></div>
+	<fieldset class="col-md-6" style="width: 100%;"><legend>Vos coordonnées</legend>
+		<div class="form-group"><label for="nom">Nom :</label><input class="form-control input_cmd" type="text" id="nom" name="nom" tabindex="1" placeholder="Votre Nom" /></div>
 		<div class="form-group"><label for="email">Email :</label><input class="form-control input_cmd" type="text" id="email" name="email" tabindex="2" placeholder="anything@[APP_NAME].appspotmail.com" /></div>
 	</fieldset>
  	<div class="clearfix"></div>
  	<br>
-	<fieldset class="col-md-6"><legend>Votre message :</legend>
-		<div class="form-group"><label for="objet">Objet :</label><input class="form-control input_cmd" type="text" id="objet" name="objet" tabindex="3" /></div>
-		<div class="form-group"><label for="message">Message :</label><textarea class="form-control input_cmd" id="message" name="message" tabindex="4" cols="30" rows="8"></textarea></div>
+	<fieldset class="col-md-6" style="width: 100%;"><legend>Votre message :</legend>
+		<div class="form-group"><label for="objet">Objet :</label><input class="form-control input_cmd" type="text" id="objet" name="objet" tabindex="3" placeholder="Vous voulez faire quoi ?" /></div>
+		<div class="form-group"><label for="message">Message :</label><textarea class="form-control input_cmd" id="message" name="message" tabindex="4" cols="30" rows="8" placeholder="Un petit message pour le SPDE - IoT"></textarea></div>
 	</fieldset>
  	<div class="clearfix"></div>
  	<br>
-	<div style="text-align:center;"><button class="btn btn-sucess" type="submit" name="envoi">Envoyer ma commande</button></div>
+	<div style="text-align:center;"><button class="btn btn-default" type="submit" name="envoi">Envoyer ma commande</button></div>
 	</form> <!-- end form -->
 
 </div><!-- end panel body -->
