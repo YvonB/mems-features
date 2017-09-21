4bb7611ffb27019d42fda6cbc7823946bc43955b
Kevin <yvonbenahita@gmail.com>
2017-09-21 16:34:28 +0300

    presque

--- zone_2/views/Contact/contact.php
+++ zone_2/views/Contact/contact.php
@@ -16,20 +16,22 @@
 <br>
 <div class="panel panel-default">
 
-	<div class="panel-heading">Envoyer nous votre commande</div>
+	<div class="panel-heading" style="font-size: 20px;">Envoyer nous votre commande</div>
 	<div class="panel-body">
 	<form id="contact" method="post" action="/contact_action">
 	<fieldset class="col-md-6"><legend>Vos coordonnées</legend>
-		<div class="form-group"><label for="nom">Nom :</label><input class="form-control" type="text" id="nom" name="nom" tabindex="1" /></div>
-		<div class="form-group"><label for="email">Email :</label><input class="form-control" type="text" id="email" name="email" tabindex="2" placeholder="anything@[APP_NAME].appspotmail.com" /></div>
+		<div class="form-group"><label for="nom">Nom :</label><input class="form-control input_cmd" type="text" id="nom" name="nom" tabindex="1" /></div>
+		<div class="form-group"><label for="email">Email :</label><input class="form-control input_cmd" type="text" id="email" name="email" tabindex="2" placeholder="anything@[APP_NAME].appspotmail.com" /></div>
 	</fieldset>
  	<div class="clearfix"></div>
+ 	<br>
 	<fieldset class="col-md-6"><legend>Votre message :</legend>
-		<div class="form-group"><label for="objet">Objet :</label><input class="form-control" type="text" id="objet" name="objet" tabindex="3" /></div>
-		<div class="form-group"><label for="message">Message :</label><textarea class="form-control" id="message" name="message" tabindex="4" cols="30" rows="8"></textarea></div>
+		<div class="form-group"><label for="objet">Objet :</label><input class="form-control input_cmd" type="text" id="objet" name="objet" tabindex="3" /></div>
+		<div class="form-group"><label for="message">Message :</label><textarea class="form-control input_cmd" id="message" name="message" tabindex="4" cols="30" rows="8"></textarea></div>
 	</fieldset>
  	<div class="clearfix"></div>
-	<div style="text-align:center;"><button class="btn btn-sucess" type="submit" name="envoi">Envoyer ma commande !</button></div>
+ 	<br>
+	<div style="text-align:center;"><button class="btn btn-sucess" type="submit" name="envoi">Envoyer ma commande</button></div>
 	</form> <!-- end form -->
 
 </div><!-- end panel body -->
