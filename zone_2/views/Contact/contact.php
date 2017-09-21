79a58d8405b61ad803f3b98355c44c91ce55cf3f
Kevin <yvonbenahita@gmail.com>
2017-09-21 16:19:53 +0300

    design du formulaire de commande

--- zone_2/views/Contact/contact.php
+++ zone_2/views/Contact/contact.php
@@ -3,21 +3,40 @@
 <head>
 	<meta charset="UTF-8">
 	<title>SDPE - IoT Order</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <meta name="author" content="Yvon Benahita">
+    <link rel="icon" type="image/png" href="/img/datastore-logo.png" />
+	<link rel="stylesheet" type="text/css" href="/css/demo.css">
 </head>
 <body>
+<div class="container">
+<br>
+<div class="panel panel-default">
+
+	<div class="panel-heading">Envoyer nous votre commande</div>
+	<div class="panel-body">
 	<form id="contact" method="post" action="/contact_action">
-	<fieldset><legend>Vos coordonnées</legend>
-		<p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" tabindex="1" /></p>
-		<p><label for="email">Email :</label><input type="text" id="email" name="email" tabindex="2" placeholder="anything@[APP_NAME].appspotmail.com" /></p>
+	<fieldset class="col-md-6"><legend>Vos coordonnées</legend>
+		<div class="form-group"><label for="nom">Nom :</label><input class="form-control" type="text" id="nom" name="nom" tabindex="1" /></div>
+		<div class="form-group"><label for="email">Email :</label><input class="form-control" type="text" id="email" name="email" tabindex="2" placeholder="anything@[APP_NAME].appspotmail.com" /></div>
 	</fieldset>
- 
-	<fieldset><legend>Votre message :</legend>
-		<p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" tabindex="3" /></p>
-		<p><label for="message">Message :</label><textarea id="message" name="message" tabindex="4" cols="30" rows="8"></textarea></p>
+ 	<div class="clearfix"></div>
+	<fieldset class="col-md-6"><legend>Votre message :</legend>
+		<div class="form-group"><label for="objet">Objet :</label><input class="form-control" type="text" id="objet" name="objet" tabindex="3" /></div>
+		<div class="form-group"><label for="message">Message :</label><textarea class="form-control" id="message" name="message" tabindex="4" cols="30" rows="8"></textarea></div>
 	</fieldset>
- 
-	<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer ma commande !" /></div>
-</form>
+ 	<div class="clearfix"></div>
+	<div style="text-align:center;"><button class="btn btn-sucess" type="submit" name="envoi">Envoyer ma commande !</button></div>
+	</form> <!-- end form -->
+
+</div><!-- end panel body -->
+</div> <!-- end panel default -->
+</div> <!-- end container -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 
